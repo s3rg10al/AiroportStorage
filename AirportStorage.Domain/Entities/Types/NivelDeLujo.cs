d676c3c35f4e96480b0febe369af1f7635168d3e
Sergio <sergioalvarezalmeida@gmail.com>
2024-02-26 22:03:08 -0500

    Funcionnalidad lista: clase aviones y enumerados

---
+++ AirportStorage.Domain/Entities/Types/NivelDeLujo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportStorage.Domain.Entities.Types
+{
+    /// <summary>
+    /// nivel de lujo que presenta un jet
+    /// </summary>
+    public enum NivelDeLujo
+    {
+        /// <summary>
+        /// alto, clasificacion del nuvel de lujo de los jets
+        /// </summary>
+        Alto,
+        /// <summary>
+        /// medio, clasificacion del nuvel de lujo de los jets
+        /// </summary>
+        Medio,
+        /// <summary>
+        /// estanar, clasificacion del nuvel de lujo de los jets
+        /// </summary>
+        Estandar,
+
+
+        // el mas basico es estandar porque un jet privado ya trae implicito cierto lujo
+    }
+}
---
+++ AirportStorage.Domain/Entities/Types/Propiety.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportStorage.Domain.Entities.Types
+{
+    /// <summary>
+    /// Propiedad de la Comapañia
+    /// </summary>
+    public enum Propiety //TODO:Poner todas las propiedades en mayuscula
+    {
+       /// <summary>
+       /// Privada, Tipo de Propiedad
+       /// </summary>
+        Privada,
+        /// <summary>
+        /// Estatal, Tipo de Propiedad
+        /// </summary>
+        Estatal,
+    }
+}
