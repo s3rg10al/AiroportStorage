08e9d33e6af793f4768dcab1907432aee93660ed
HernanDPV41 <hernandpv41@gmail.com>
2024-03-19 23:18:47 -0400

    Fix migration errors.

---
+++ AirportStorage.DataAccess/Migrations/20240320031729_Initial.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirportStorage.DataAccess.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Pais = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hangar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CompanyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hangar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CantJets = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Adress = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumb = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Planes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Estado = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantKmsTotales = table.Column<uint>(type: "INTEGER", nullable: false),
+                    CantKmsDesdeMant = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Staff",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Exp = table.Column<int>(type: "INTEGER", nullable: false),
+                    Years = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Cargo = table.Column<string>(type: "TEXT", nullable: false),
+                    Sueldo = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Adress = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumb = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staff", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Store",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CantPiezas = table.Column<uint>(type: "INTEGER", nullable: false),
+                    UltimoInv = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    WorkshopId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Store", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workshop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StoreId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CompanyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ability = table.Column<uint>(type: "INTEGER", nullable: false),
+                    IsAbility = table.Column<bool>(type: "INTEGER", nullable: false),
+                    specialty = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workshop", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commercials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HangarId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commercials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commercials_Planes_Id",
+                        column: x => x.Id,
+                        principalTable: "Planes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LuxuryLevel = table.Column<int>(type: "INTEGER", nullable: false),
+                    OwnerId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jets_Planes_Id",
+                        column: x => x.Id,
+                        principalTable: "Planes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssuranceStaff",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CantHorasTrabajadas = table.Column<uint>(type: "INTEGER", nullable: false),
+                    CantHorasExtras = table.Column<uint>(type: "INTEGER", nullable: false),
+                    PagoxHoras = table.Column<uint>(type: "INTEGER", nullable: false),
+                    PagoxHorasExtras = table.Column<uint>(type: "INTEGER", nullable: false),
+                    HangarId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssuranceStaff", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssuranceStaff_Staff_Id",
+                        column: x => x.Id,
+                        principalTable: "Staff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mechanic",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CantRep = table.Column<uint>(type: "INTEGER", nullable: false),
+                    PagoxRep = table.Column<uint>(type: "INTEGER", nullable: false),
+                    WorkshopId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mechanic", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mechanic_Staff_Id",
+                        column: x => x.Id,
+                        principalTable: "Staff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssuranceStaff");
+
+            migrationBuilder.DropTable(
+                name: "Commercials");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropTable(
+                name: "Hangar");
+
+            migrationBuilder.DropTable(
+                name: "Jets");
+
+            migrationBuilder.DropTable(
+                name: "Mechanic");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+
+            migrationBuilder.DropTable(
+                name: "Store");
+
+            migrationBuilder.DropTable(
+                name: "Workshop");
+
+            migrationBuilder.DropTable(
+                name: "Planes");
+
+            migrationBuilder.DropTable(
+                name: "Staff");
+        }
+    }
+}
