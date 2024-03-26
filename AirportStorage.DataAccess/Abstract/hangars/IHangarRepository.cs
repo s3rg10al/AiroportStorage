bd34d63b691a9d978bd047d35f0761b06f026e98
kevin <kevin@DESKTOP-27G13FV>
2024-03-26 00:34:59 -0400

    patron repositorio implementado

---
+++ AirportStorage.DataAccess/Abstract/hangars/IHangarRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirportStorage.Domain.Entities.Hangar;
+
+namespace AirportStorage.DataAccess.Abstract.Hangar
+{
+    public interface IHangarRepository : IRepository
+    {
+        /// <summary>
+        /// Crea un hangar en BD
+        /// <summary>
+        /// constructor de la clase Hangar 
+        /// </summary>
+        /// <param name="InitialCantMax">se debe iniciar la cantidad maxima de aviones</param>
+        Hangar Create(int InitialCantMax); //pregunta tambien lleva el id de company
+
+        /// <summary>
+        /// Obtiene un hangar
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Hangar? Get(int id);
+
+        /// <summary>
+        /// Actualiza los parametros de un hangar en BD
+        /// </summary>
+        /// <param name="Hangar"></param>
+        void Update(Hangar hangars);
+
+        /// <summary>
+        /// Elima un hangar de BD
+        /// </summary>
+        /// <param name="Hangar"></param>
+        void Delete(Hangar hangars);
+
+    }
+}
---
+++ AirportStorage.DataAccess/Repositories/hangars/HangarRepository.cs
@@ -0,0 +1,40 @@
+﻿using AirportStorage.Domain.Entities.Hangar;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AirportStorage.DataAccess.Repositories
+{
+    public partial class ApliccationRepository : IHangarRepository
+    {
+        Hangar Create(int InitialCantMax)
+        {
+            Hangar hangars = new Hangar(InitialCantMax);
+            _context.Add(hangars);
+            return hangars;
+        }
+
+
+        Hangar? Get(int id)
+        {
+            return _context.Set<Hangar>().Find(id);
+        }
+
+
+        void Update(Hangar hangars)
+        {
+            _context.Update(hangars);
+        }
+
+
+        void Delete(Hangar hangars)
+        {
+            _context.Remove(hangars);
+        }
+    }
+}
