b9a89d71bd8ade68c96990a07acf7e80c1c0d7ba
kevin <kevin@DESKTOP-27G13FV>
2024-03-26 14:46:31 -0400

    unitarias hechas ya

--- AirportStorage.DataAcces.Tests/CompanyTests.cs
+++ AirportStorage.DataAcces.Tests/CompanyTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,97 @@ using System.Threading.Tasks;
 
 namespace AirportStorage.DataAcces.Tests
 {
-    internal class CompanyTests
+    public class CompanyTests
     {
+        private ICompanyRepository _companyRepository;
+
+        
+        public CompanyTests()
+        {
+            _companyRepository = new ApplicationRepository(ConnectionStringProvider.GetConnectionString());
+        }
+        
+        [DataRow(cuba)]
+        [DataRow(USA)]
+        [TestMethod]
+        public void Can_Create_Company(string InitialCountry)
+        {
+            //Arrange
+            _companyRepository.BeginTransaction();
+
+            //Execute
+            Company newCompany = _companyRepository.Create(InitialCountry);
+            _companyRepository.PartialCommit();  //generando id 
+            Company? loadedCompany = _companyRepository.Get(newCompany.Id);
+            _companyRepository.CommitTransaction();
+
+            //Assert
+            Assert.IsNotNull(loadedCompany);
+            Assert.AreEqual(loadedCompany.Currency, InitialCountry);
+            
+
+        }
+
+
+        [DataRow(1)]
+        [DataRow(2)]
+        [TestMethod]
+        public void Can_Get_Company(int id)
+        {
+            //Arrange 
+            _companyRepository.BeginTransaction();
+
+            // Execute
+            var loadedCompany = _companyRepository.Get(id);
+            _companyRepository.CommitTransaction();
+
+            //Assert
+            Assert.IsNotNull(loadedCompany);
+        }
+
+        [DataRow(1,cuba)]
+        [DataRow(2, USA)]
+        [TestMethod]
+        public void Can_Update_Company(int id,string InitialCountry)
+        {
+            //Arrange
+            _companyRepository.BeginTransaction();
+
+            //Execute
+            var loadedCompany = _companyRepository.Get(id);
+            Assert.IsNotNull(loadedCompany);
+            var newCompany = new Company(InitialCountry) { id = loadedCompany.Id };
+            _companyRepository.Update(newCompany);
+            var modifyedCompany = _companyRepository.Get(id);
+            _companyRepository.CommitTransaction();
+
+            //Assert
+            Assert.AreEqual(modifyedCompany.Currency, InitialCountry);
+          
+
+
+        }
+
+        [DataRow(1)]
+        [TestMethod]
+        public void Can_Delete_Company(int id)
+        {
+            //Arrange
+            _companyRepository.BeginTransaction();
+
+            //Execute
+            var loadedCompany = _companyRepository.Get(id);
+            Assert.IsNotNull(loadedCompany);
+            _companyRepository.Delete(loadedCompany);
+            _companyRepository.PartialCommit();
+            loadedCompany = _companyRepository.Get(id);
+            _companyRepository.CommitTransaction();
+
+            //Assert
+            Assert.IsNull(loadedCompany);
+
+
+
+        }
     }
 }
--- AirportStorage.DataAcces.Tests/HangarTests.cs
+++ AirportStorage.DataAcces.Tests/HangarTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,97 @@ using System.Threading.Tasks;
 
 namespace AirportStorage.DataAcces.Tests
 {
-    internal class HangarTests
+    public class HangarTests
     {
+        private IHangarRepository _hangarRepository;
+
+
+        public HangarTests()
+        {
+            _hangarRepository = new ApplicationRepository(ConnectionStringProvider.GetConnectionString());
+        }
+
+        [DataRow(200)]
+        [DataRow(300)]
+        [TestMethod]
+        public void Can_Create_Hangar(InitialCantMax )
+        {
+            //Arrange
+            _companyRepository.BeginTransaction();
+
+            //Execute
+            Hangar newHangar = _HangarRepository.Create(InitialCantMax);
+            _hangarRepository.PartialCommit();  //generando id 
+            Hangar? loadedHangar = _hangarRepository.Get(newHangar.Id);
+            _HangarRepository.CommitTransaction();
+
+            //Assert
+            Assert.IsNotNull(loadedHangar);
+            Assert.AreEqual(loadedHangar.Currency, InitialCantMax);
+
+
+        }
+
+
+        [DataRow(1)]
+        [DataRow(2)]
+        [TestMethod]
+        public void Can_Get_Hangar(int id)
+        {
+            //Arrange 
+            _hangarRepository.BeginTransaction();
+
+            // Execute
+            var loadedHangar = _hangarRepository.Get(id);
+            _hangarRepository.CommitTransaction();
+
+            //Assert
+            Assert.IsNotNull(loadedHangar);
+        }
+
+        [DataRow(1, 200)]
+        [DataRow(2, 300)]
+        [TestMethod]
+        public void Can_Update_Hangar(int id, int InitialCantMax)
+        {
+            //Arrange
+            _hangarRepository.BeginTransaction();
+
+            //Execute
+            var loadedHangar = _hangarRepository.Get(id);
+            Assert.IsNotNull(loadedHangar);
+            var newHangar = new Hangar(InitialCantMax) { id = loadedHangar.Id };
+            _hangarRepository.Update(newHangar);
+            var modifyedHangar = _HangarRepository.Get(id);
+            _hangarRepository.CommitTransaction();
+
+            //Assert
+            Assert.AreEqual(modifyedHangar.Currency, InitialCantMax);
+
+
+
+        }
+
+        [DataRow(1)]
+        [TestMethod]
+        public void Can_Delete_Hangar(int id)
+        {
+            //Arrange
+            _hangarRepository.BeginTransaction();
+
+            //Execute
+            var loadedHangar = _hangarRepository.Get(id);
+            Assert.IsNotNull(loadedHangar);
+            _hangarRepository.Delete(loadedHangar);
+            _hangarRepository.PartialCommit();
+            loadedHangar = _hangarRepository.Get(id);
+            _hangarRepository.CommitTransaction();
+
+            //Assert
+            Assert.IsNull(loadedHangar);
+
+
+
+        }
     }
 }
